171f3687d90a3d5a19bb6e653f0541b66fb215fc
Daria Diachkova <daria.diachkova@mail.ru>
2021-10-14 16:32:58 +0400

    feat(app): add translations

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,29 @@
+import React, { ComponentType } from 'react'
+import Head from 'next/head'
+import { AppProps } from 'next/app'
+
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { normalize } from 'styled-normalize'
+
+import { appWithTranslation, useTranslation } from 'next-i18next'
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
+ ${normalize}
+
+ * {
     box-sizing: border-box;
   }
+  html {
+    height: 100%;
+    font-family: Karla, Arial, sans-serif;
+    white-space: pre-line;
+    -webkit-font-smoothing: antialiased;
+  }
 `
+
+type ApplicationProps = AppProps & {
+  Component: AppProps['Component'] & { Layout?: React.FC }
+}
 
 const theme = {
   colors: {
@@ -14,13 +31,39 @@ const theme = {
   },
 }
 
-export default function App({ Component, pageProps }) {
+const App: React.FC<ApplicationProps> = ({ Component, pageProps }) => {
+  const Layout: ComponentType = Component.Layout || React.Fragment
+  const { t } = useTranslation(['common'])
+
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>{t('app_title')}</title>
+        {/* <meta name="description" content={t('app_description')} /> */}
+        {/* <meta
+          name="og:image"
+          property="og:image"
+          content="/assets/share.png"
+        /> */}
+        {/* <meta name="twitter:image" content="/assets/share-avatar-v2.png" /> */}
+        <link rel="manifest" href="/favicon/site.webmanifest.json" />
+        <link rel="icon" href="/favicon/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
+      </Head>
+        <ThemeProvider theme={theme}>
+          {Component.Layout ? (
+            <Layout {...pageProps}>
+              <Component {...pageProps} />
+            </Layout>
+          ) : (
+            <Component {...pageProps} />
+          )}
+        </ThemeProvider>
       <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
     </>
   )
 }
+
+export default appWithTranslation(App)
