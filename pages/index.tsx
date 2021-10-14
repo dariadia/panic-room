87d0b4c906e44dfc9c8dca6321e62b3534bac854
Daria Diachkova <daria.diachkova@mail.ru>
2021-10-14 18:35:08 +0400

    feat(app): add theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,25 @@
-import styled from 'styled-components'
+import React from 'react'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import type { Locale, Page, SinglePage as SinglePageProps } from 'types'
 
-export default function Home() {
-  return <Title>Hello World</Title>
+const HomePage: Page<SinglePageProps> = () => {
+  return <h1>Hello World</h1>
 }
+
+export async function getStaticProps({
+  locale,
+}: {
+  locale: Locale
+}): Promise<{ props: SinglePageProps }> {
+  return {
+    props: {
+      locale,
+      ...(await serverSideTranslations(locale, [
+        'common',
+      ])),
+    },
+  }
+}
+
+export default HomePage
--- utils/theme.ts
+++ utils/theme.ts
@@ -1,5 +1,20 @@
+const lightTheme = {
+  text: '#302119',
+  alert: '#4c1130',
+  accent: '#073763',
+  background: '#fafbfc',
+}
+
+const darkTheme = {
+  text: '#fafbfc',
+  alert: '#4c1130',
+  accent: '#76a5af', // or try #d0e0e3
+  background: '#302119',
+}
+
+export const isDarkMode = () => window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+
 export const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
+  lightTheme,
+  darkTheme,
 }
