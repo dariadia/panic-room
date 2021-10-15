24e4a85394da569c52acb7b3c4e5c75137c5e4eb
Daria Diachkova <daria.diachkova@mail.ru>
2021-10-15 19:43:55 +0400

    feat(index): add WelcomeScreen styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,39 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { useContext } from 'react'
+import styled, { ThemeContext } from 'styled-components'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 import { hasUserPreferences } from 'utils/theme'
 import { MainLayout } from '@/layouts'
 
-import type { Locale, Page, SinglePage as SinglePageProps } from 'types'
+import type { Locale, Page, SinglePage as SinglePageProps, Theme } from 'types'
 
 const HomePage: Page<SinglePageProps> = () => {
-  let hasSavedPreferences = false
+  const hasSavedPreferences = hasUserPreferences()
 
-  hasSavedPreferences = hasUserPreferences()
+  const { darkModeActive, theme } = useContext(ThemeContext)
 
   return hasSavedPreferences ? (
     <MainScreen />
   ) : (
-    <WelcomeScreen>
+    <WelcomeScreen theme={darkModeActive ? theme.darkTheme : theme.lightTheme}>
       <WelcomeMessage />
     </WelcomeScreen>
   )
 }
 
-const WelcomeMessage = () => <div>welcome</div>
+const WelcomeMessage = () => <span>welcome</span>
 
-const WelcomeScreen = styled('div')``
+const WelcomeScreen: React.FC<{ theme: Theme }> = styled('div')<{
+  theme: Theme
+}>`
+  min-width: 260px;
+  width: fit-content;
+  max-width: calc(100% - 32px);
+  min-height: 70vh;
+  margin: auto;
+  filter: drop-shadow(1px 2px 8px hsl(220deg 60% 50%));
+  background: ${({ theme }) => theme.accent};
+`
 
 const MainScreen = () => <div>main</div>
 
