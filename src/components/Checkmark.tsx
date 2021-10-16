26faab2878d522aa56d03ed30ac9a143021bdef3
Daria Diachkova <daria.diachkova@mail.ru>
2021-10-16 12:19:48 +0400

    feat(checkmark): style

--- src/components/Checkmark.tsx
+++ src/components/Checkmark.tsx
@@ -1,4 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
 
-export const Checkmark: React.FC = styled('span')``
+export const Checkmark: React.FC = styled('span')`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 25px;
+  width: 25px;
+  background-color: #eee;
+  :after {
+    content: '';
+    position: absolute;
+    display: none;
+  }
+`
