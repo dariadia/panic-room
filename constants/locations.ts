7423c4790b31865c95c52ad1f1ec6e4352f1909a
Daria Diachkova <daria.diachkova@mail.ru>
2021-11-07 08:47:28 +0300

    feat(consts): add new

--- constants/locations.ts
+++ constants/locations.ts
@@ -2,3 +2,7 @@ export const DANNI_GITHUB = 'https://github.com/dariadia/'
 export const DANNI_LOGIN = 'dariadia'
 
 export const AVAILABLE_ROOMS = ['fortune-cookie']
+
+export const FORTUNE_COOKIES_PATH_ALL = '/api/fortune-cookie-wishes'
+export const FORTUNE_COOKIES_PATH_COUNT = '/api/fortune-cookie-counter'
+export const FORTUNE_COOKIES_PATH_ONE = '/api/fortune-cookie/'
