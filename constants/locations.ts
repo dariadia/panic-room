0756b85eff504fdfdc2895443fbf98df43258b5d
Daria Diachkova <daria.diachkova@mail.ru>
2021-11-07 09:08:19 +0300

    feat(consts): add new

--- constants/locations.ts
+++ constants/locations.ts
@@ -3,6 +3,8 @@ export const DANNI_LOGIN = 'dariadia'
 
 export const AVAILABLE_ROOMS = ['fortune-cookie']
 
-export const FORTUNE_COOKIES_PATH_ALL = '/api/fortune-cookie-wishes'
-export const FORTUNE_COOKIES_PATH_COUNT = '/api/fortune-cookie-counter'
-export const FORTUNE_COOKIES_PATH_ONE = '/api/fortune-cookie/'
+export const FORTUNE_COOKIES_PATH_ALL = '/fortune-cookie-wishes'
+export const FORTUNE_COOKIES_PATH_COUNT = '/fortune-cookie-counter'
+export const FORTUNE_COOKIES_PATH_ONE = '/fortune-cookie/'
+
+export const APP_PRODUCTION = 'http://panic-room.vercel.app/'
