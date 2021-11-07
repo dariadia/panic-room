291be826221fc8c3fe44344af997f4645db4fbcb
Daria Diachkova <daria.diachkova@mail.ru>
2021-11-07 08:41:51 +0300

    fix(api): params

--- pages/api/fortune-cookie-wishes.ts
+++ pages/api/fortune-cookie-wishes.ts
@@ -1,9 +1,20 @@
 import fortuneCookieWishes from './fortune-cookie-wishes.json'
+import { FortuneCookie } from 'types'
 
-export default function handler(res: {
-  status: (
-    arg0: number,
-  ) => { (): any; new (): any; json: { (arg1: any): void; new (): any } }
-}): void {
+export default function handler(
+  req: any,
+  res: {
+    status: (
+      arg0: number,
+    ) => {
+      (): any
+      new (): any
+      json: {
+        (arg1: FortuneCookie[]): void
+        new (): any
+      }
+    }
+  },
+): void {
   res.status(200).json(fortuneCookieWishes)
 }
