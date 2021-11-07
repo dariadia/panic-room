66d16b75365f1cbca07e1d814a8c62703c2e9e93
Daria Diachkova <daria.diachkova@mail.ru>
2021-11-07 09:07:58 +0300

    feat(hooks): add new for api

---
+++ hooks/use-api.ts
@@ -0,0 +1,37 @@
+import useSWR from 'swr'
+import { Fetcher, SWRResponse } from 'swr/dist/types'
+
+import { APP_PRODUCTION } from 'constants/locations'
+
+import { RequestOptions, RequestOption, OptionsType } from 'types'
+
+const buildRequestUrl = (url: string, RequestParams?: RequestOptions) => {
+  if (!RequestParams) {
+    return url
+  }
+  const queryString = Object.keys(RequestParams)
+    .map(key => `${key}=${RequestParams[key as RequestOption]}`)
+    .join('&')
+  return `${url}?${queryString}`
+}
+
+const getDefaultFetcher = (url: string) => () =>
+  fetch(url).then(res => res.json())
+
+export const useAPI = ({
+  url,
+  fetcher,
+  options,
+  requestParams,
+  host = APP_PRODUCTION,
+}: {
+  url: string
+  fetcher?: Fetcher<unknown>
+  options?: OptionsType
+  requestParams?: RequestOptions
+  host?: string
+}): SWRResponse<unknown, unknown> => {
+  const apiUrl = `${host}/api${buildRequestUrl(url, requestParams)}`
+  const apiFetcher = (fetcher || getDefaultFetcher(apiUrl)) as Fetcher<unknown>
+  return useSWR(apiUrl, apiFetcher, options)
+}
