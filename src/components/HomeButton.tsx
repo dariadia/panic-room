4279aeeba6216a909bec74456107a80742e7e06f
Daria Diachkova <daria.diachkova@mail.ru>
2021-11-11 21:30:13 +0300

    fix(home-button): on client show

--- src/components/HomeButton.tsx
+++ src/components/HomeButton.tsx
@@ -4,12 +4,17 @@ import styled from 'styled-components'
 
 import { TEXTS } from 'constants/texts'
 import { HOME } from 'constants/locations'
+import { isClient } from 'utils/env'
 
-export const HomeButton: React.FC = () => (
-  <Link href={HOME}>
-    <StyledButton>🏡 {TEXTS.home}</StyledButton>
-  </Link>
-)
+export const HomeButton: React.FC | null = () => {
+  if (isClient() && window.location.pathname === HOME) return null
+
+  return (
+    <Link href={HOME}>
+      <StyledButton>🏡 {TEXTS.home}</StyledButton>
+    </Link>
+  )
+}
 
 const StyledButton = styled('button')`
   cursor: pointer;
