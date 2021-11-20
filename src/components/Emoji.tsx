4a0324da914283cf3501b178704a475b64b3d1b6
Daria Diachkova <daria.diachkova@mail.ru>
2021-11-20 17:41:24 +0300

    feat(Emoji): extend props

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 type Props = {
   label: string
+  className?: string
 }
-export const Emoji: React.FC<Props> = ({ children, label }) => (
-  <span role="img" aria-label={label}>
+export const Emoji: React.FC<Props> = ({ children, label, className }) => (
+  <span role="img" aria-label={label} className={className}>
     {children}
   </span>
 )
