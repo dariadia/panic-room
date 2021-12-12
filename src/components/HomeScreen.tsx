6ade85f92dac9cdf9327cb71d7a5d6a190b1d16f
Daria Diachkova <daria.diachkova@mail.ru>
2021-12-12 09:39:24 +0300

    Revert "feat(app): extend sample home"
    This reverts commit b3e6b4a37f79880f63bdfb3fb120eaedce6eb2a8.

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -1,17 +1,8 @@
 import React from 'react'
 
-import { ALLOW_MOTION } from 'constants/theme'
-import { getValueFromCookieString } from 'utils/theme'
-
-// import { Preferences, Theme } from 'types'
-
 export const HomeScreen: React.FC<{ preferences: string }> = ({
   preferences,
 }) => {
-  const allowMotion = getValueFromCookieString({
-    cookie: preferences,
-    value: ALLOW_MOTION,
-  })
-  console.log(allowMotion)
+  console.log(preferences)
   return <div>hello world</div>
 }
