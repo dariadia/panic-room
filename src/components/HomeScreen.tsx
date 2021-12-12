b3e6b4a37f79880f63bdfb3fb120eaedce6eb2a8
Daria Diachkova <daria.diachkova@mail.ru>
2021-12-12 09:26:44 +0300

    feat(app): extend sample home

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
+import { ALLOW_MOTION } from 'constants/theme'
+import { getValueFromCookieString } from 'utils/theme'
+
+// import { Preferences, Theme } from 'types'
+
 export const HomeScreen: React.FC<{ preferences: string }> = ({
   preferences,
 }) => {
-  console.log(preferences)
+  const allowMotion = getValueFromCookieString({
+    cookie: preferences,
+    value: ALLOW_MOTION,
+  })
+  console.log(allowMotion)
   return <div>hello world</div>
 }
